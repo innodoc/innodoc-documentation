82d7e1a74ae0ac0f3beaecc398ad741b3ba6ad5f
Niklas P <contact@niklasplessing.net>
2017-04-07 18:08:22 +0200

    add settings and logo

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Example file for course settings
+# If you remove it or change it, probably some tests will fail. Have that in mind.
+
+# Define a new setting in the base plugin
+BASE_COURSE_SETTING = 'new value'
+
+# Overwrite the course title
+description = 'Basis-Beispielmodul'
